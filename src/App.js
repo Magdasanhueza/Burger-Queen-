eba393f41ec71c412a4b602c365bc761754a8cff
ni.colara@hotmail.com <ni.colara@hotmail.com>
2021-09-26 14:49:57 -0300

    pruebas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { Button } from "react-bootstrap";
+import { ReactComponent as Icon } from "./assets/burguer.svg";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hola Nico</h1>
+      <Button variant="danger">Danger</Button> <Icon />
     </div>
   );
 }
